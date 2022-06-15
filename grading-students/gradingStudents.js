382475e2aecd280556e04c5e3ddf45950f10422b
vinicius guterres <vinicius@spacecom.com.br>
2022-06-15 18:38:05 -0300

    add grading students challenge

---
+++ grading-students/gradingStudents.js
@@ -0,0 +1,26 @@
+function gradingStudents(grades) {
+
+    const roundToNextMultipleOfFive = (number) => {
+        return Math.ceil(number / 5) * 5;
+    }
+    
+    const roundedGrades = grades.map(grade => {
+        // Case grade is less than 40, don't round it
+        if (grade < 38) {
+            return grade;
+        }
+        
+        // case bigger than 40, get the next multiple of 5
+        const roundedGrade = roundToNextMultipleOfFive(grade);
+
+        if ((roundedGrade - grade) < 3) {
+            return roundedGrade;
+        }
+        
+        return grade;
+    });
+
+    return roundedGrades;
+}
+
+console.log(gradingStudents([ 73, 67, 38, 33 ]));
