3167533dd67e0318ac259eebfbaf68e2065acbde
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-10-07 21:16:32 -0300

    Improve code readbility

--- beautiful-days-at-the-movies/solution2.js
+++ beautiful-days-at-the-movies/solution2.js
@@ -1,17 +1,30 @@
 function beautifulDays(initialDay, finalDay, divisor) {
+    // Setting my range of days based on the initial and the final day
     const myRangeOfDays = Array.from({ length: finalDay - initialDay + 1 },
         (_, i) => initialDay + i);
 
+    // Filtering the beautiful days based on the math clauses
     const myBeautifulDaysArr = myRangeOfDays.filter(currentDay => {
-        const reverse = parseInt(currentDay.toString().split('').reverse().join(''));
-        const reverseAndCurrentDayDifference = Math.abs(reverse - currentDay);
+        const reversedNumber = reverse(currentDay);
+        const reverseAndCurrentDayDifference = Math.abs(reversedNumber - currentDay);
 
         if (reverseAndCurrentDayDifference % divisor === 0) {
             return currentDay;
-        }        
+        }
     });
 
     return myBeautifulDaysArr.length;
+}
+
+function reverse(myNumber) {
+    const reversedStr =
+        myNumber
+            .toString()
+            .split('')
+            .reverse()
+            .join('');
+
+    return parseInt(reversedStr)
 }
 
 // Input 20, 23, 6
