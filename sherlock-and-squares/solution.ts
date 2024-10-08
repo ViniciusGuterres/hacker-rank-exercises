0c881c0ee6d840ceefbbb736d79d41badf0e34de
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-10-08 10:54:31 -0300

    Added sherlock and squares challenge

---
+++ sherlock-and-squares/solution.ts
@@ -0,0 +1,12 @@
+function squares(a: number, b: number): number {
+    return 3;
+}
+
+
+const a = 24;
+const b = 49;
+
+const result = squares(24, 49);
+
+// Expect output: 3
+console.log("🚀 ~ result:", result)
