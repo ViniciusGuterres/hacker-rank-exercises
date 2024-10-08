8cafe95f25d2d1e0749427caa955f6d78ab93298
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-10-07 21:02:26 -0300

    Refactoring the solution

--- migratory-birds/solution.js
+++ migratory-birds/solution.js
@@ -2,33 +2,22 @@ function migratoryBirds(arr) {
     const birdsSighted = {};
 
     arr.forEach(bird => {
-        if (!birdsSighted[bird]) {
-            birdsSighted[bird] = 1;
-        } else {
-            birdsSighted[bird]++;
-        }
+        birdsSighted[bird] = (birdsSighted[bird] || 0) + 1;
     });
+        
+    let maxSightedBirdID = parseInt(Object.keys(birdsSighted)[0]);
+    let maxSightedBirdCounterValue = birdsSighted[maxSightedBirdID];
 
-    
-    const birdsValues = Object.entries(birdsSighted);
-    let maxSightedBirdID = birdsValues[0][0];
-    
-    for (let i = 0; i < birdsValues.length; i++) {
-        const [birdID, birdSighCounter] = birdsValues[i];
-
-        const maxSightedBirdCounterValue = birdsSighted[maxSightedBirdID];
-
-        if (birdSighCounter > maxSightedBirdCounterValue) {
-            maxSightedBirdID = birdID;
-            continue;
-        }
+    for (let birdIDStr in birdsSighted) {
+        const birdSightCounter = birdsSighted[birdIDStr];
+        const birdID = parseInt(birdIDStr);
 
         if (
-            birdSighCounter == maxSightedBirdCounterValue 
-            && birdID < maxSightedBirdID
+            birdSightCounter > maxSightedBirdCounterValue
+            || (birdSightCounter === maxSightedBirdCounterValue && birdID < maxSightedBirdID)
         ) {
             maxSightedBirdID = birdID;
-            continue;
+            maxSightedBirdCounterValue = birdSightCounter;
         }
     }
 
